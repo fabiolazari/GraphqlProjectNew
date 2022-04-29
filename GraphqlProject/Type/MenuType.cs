0392cc10962f7bfa4806ad301560050d1cb5f8c0
Fabio Lazari Costa <lazari.developer@gmail.com>
2022-04-29 19:01:44 -0300

    Commit projeto GraphQl + .Net - Segundo(New)

--- GraphqlProject/Type/MenuType.cs
+++ GraphqlProject/Type/MenuType.cs
@@ -8,7 +8,7 @@ namespace GraphqlProject.Type
     {
         public MenuType(ISubMenu subMenuService)
         {
-            Field(m => m.Id);
+            Field(m => m.Id); 
             Field(m => m.Name);
             Field(m => m.ImageUrl);
             Field<ListGraphType<MenuType>>("subMenus", resolve: context => { return subMenuService.GetSubMenus(context.Source.Id); });
